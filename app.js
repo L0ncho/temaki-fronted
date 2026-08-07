89b9628f0a86866590c74d0f352d8c0a8259de38
JR <jesusrivasvera22@gmail.com>
2026-08-06 20:37:36 -0400

    arreglos, menu carrito, colores, opciones

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
-// Esta variable es la memoria de nuestra página
+// Memoria principal del sistema
 let carrito = [];
+let totalAcumulado = 0;
 
-// Esta función se ejecuta cada vez que alguien presiona el botón
 function agregarAlCarrito(nombreProducto, precioProducto) {
+    // 1. Agregar el producto a la memoria
+    carrito.push({ nombre: nombreProducto, precio: precioProducto });
     
-    // 1. Creamos un objeto con los datos del sushi
-    const nuevoItem = {
-        nombre: nombreProducto,
-        precio: precioProducto
-    };
+    // 2. Sumar el precio al total acumulado
+    totalAcumulado = totalAcumulado + precioProducto;
 
-    // 2. Lo empujamos dentro de nuestro arreglo del carrito
-    carrito.push(nuevoItem);
+    // 3. Ejecutar la función que dibuja esto en la pantalla
+    actualizarPantalla();
+}
 
-    // 3. Mostramos un mensaje oculto para comprobar que funciona
-    console.log("¡Sushi agregado exitosamente!");
-    console.log("Tu carrito ahora tiene:", carrito);
+function actualizarPantalla() {
+    // Capturar los elementos HTML donde vamos a escribir
+    const listaHTML = document.getElementById('lista-carrito');
+    const totalHTML = document.getElementById('total-precio');
+
+    // Limpiar la lista visual antes de volver a dibujarla para que no se duplique
+    listaHTML.innerHTML = '';
+
+    // Recorrer nuestra memoria (el arreglo) y crear un <li> por cada sushi
+    carrito.forEach(function(producto) {
+        const itemLi = document.createElement('li');
+        
+        // Escribir el nombre y el precio dentro del <li>
+        itemLi.innerText = `${producto.nombre} - $${producto.precio}`;
+        
+        // Inyectar el <li> en el HTML
+        listaHTML.appendChild(itemLi);
+    });
+
+    // Actualizar el número del precio total en el HTML
+    totalHTML.innerText = totalAcumulado;
 }
